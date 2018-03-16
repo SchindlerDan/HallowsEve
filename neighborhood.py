108b97cd7ade07a7b5fded9d0476b4d4f618de4d
Daniel Schindler <schindld@dc18.cis.gvsu.edu>
2018-03-16 19:22:15 -0400

    pulling what Emily pushed -Dan Schindler

--- neighborhood.py
+++ neighborhood.py
@@ -1,9 +1,8 @@
 class neighborhood():
-	houses = []
-	
+	houses = [[]]
+#https://stackoverflow.com/questions/6667201/how-to-define-a-two-dimensional-array-in-python	
 	def _init_(rows, columns):
-		
-	
+		houses = [[house() for x in range(columns)] for y in range(rows)];
 	
 	
 	
